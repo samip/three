2079dc2acb416eb5dc95838acb77cfff9d88c949
Sami Pellinen <sami@pell.fi>
2024-10-05 07:47:53 +0300

    Fix crash on web os (no gyroscope)

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,23 +1,35 @@
 import React, { useRef, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
- import { Gyroscope } from 'expo-sensors';
+import { Gyroscope } from 'expo-sensors';
+import { Platform } from "react-native";
 
 export default function Box() {
   const ref = useRef<THREE.Mesh>(null!);
   const rotation = useRef({ x: 0, y: 0, z: 0 });
 
   useEffect(() => {
-    const subscription = Gyroscope.addListener((data) => {
-      rotation.current.x = data.x ?? 0;
-      rotation.current.y = data.y ?? 0;
-      rotation.current.z = data.z ?? 0;
-    });
+    let subscription:any;
+
+    if (Platform.OS !== "web") {
+      // available was true on web platform for some reason
+      Gyroscope.isAvailableAsync().then((available) => {
+        if (available) {
+          subscription = Gyroscope.addListener((data) => {
+            rotation.current.x = data.x ?? 0;
+            rotation.current.y = data.y ?? 0;
+            rotation.current.z = data.z ?? 0;
+          });
+        }
+      });
+  }
 
     Gyroscope.setUpdateInterval(16); // Update interval in milliseconds
 
     return () => {
-      subscription.remove();
+      if (subscription) {
+        subscription.remove();
+      }
     };
   }, []);
 
