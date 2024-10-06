9c8051ea27dc94704b6cd0ba23b7ce31502fe528
Sami Pellinen <sami.m.pellinen@gmail.com>
2024-10-06 10:32:48 +0300

    Specify android sdk version in install script

--- tools/install_android_sdk.sh
+++ tools/install_android_sdk.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 apt-get install -y google-android-cmdline-tools-11.0-installer
-# install platform-tools including adb
-# maybe specify adb version in future (not supported by sdkmaanger)
-yes | sdkmanager "platform-tools"
+
+# Current Expo SDK version (51.0.0) uses android SDK v34
+# Update this installer if required when bumping Expo version
+# https://docs.expo.dev/versions/latest/
+yes | sdkmanager --install "platforms;android-34" "build-tools;34.0.0" "platform-tools"
 yes | sdkmanager --licenses
