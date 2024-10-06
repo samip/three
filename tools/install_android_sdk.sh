e95a2d41c23f5ab81af50f2fa4391499fdedcf40
Sami Pellinen <sami.m.pellinen@gmail.com>
2024-10-06 12:49:19 +0300

    Add ANDROID_HOME to sdk install script

--- tools/install_android_sdk.sh
+++ tools/install_android_sdk.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-apt-get install -y google-android-cmdline-tools-11.0-installer
+export ANDROID_HOME=${ANDROID_HOME:-/opt/android-sdk}
+
+apt-get install -y --reinstall google-android-cmdline-tools-11.0-installer
 
 # Current Expo SDK version (51.0.0) uses android SDK v34
 # Update this installer if required when bumping Expo version
 # https://docs.expo.dev/versions/latest/
-yes | sdkmanager --install "platforms;android-34" "build-tools;34.0.0" "platform-tools"
+yes | sdkmanager --sdk_root=$ANDROID_HOME --install "platforms;android-34" "build-tools;34.0.0" "platform-tools"
 yes | sdkmanager --licenses
+
+chown $USER $ANDROID_HOME -R
+
+echo "Add these to .bashrc or similar"
+echo "export ANDROID_HOME=$ANDROID_HOME"
+echo "export PATH=\$PATH:$ANDROID_HOME/platform-tools"
