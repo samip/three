969f1849af535ff876da7eed532d59b07ecff7f5
Sami Pellinen <sami.m.pellinen@gmail.com>
2024-10-23 10:27:11 +0300

    Change canvas bg from red to black

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import Matrix from '@/components/Matrix';
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1 }}>
-      <Canvas  style={{ flex: 1, backgroundColor: 'red' }}>
+      <Canvas  style={{ flex: 1, backgroundColor: '#3b3b3b' }}>
         <Matrix />
       </Canvas>
     </View>
