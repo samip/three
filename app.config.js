855e81248895dabb0f9f1b45eb25acb3c710688e
Sami Pellinen <sami@pell.fi>
2024-10-27 16:57:50 +0200

    Node version 20.17.0

---
+++ app.config.js
@@ -0,0 +1,16 @@
+export default ({ config }) => ({
+  ...config,
+  android: {
+    ...config.android,
+    package: "com.spell.three",
+    applicationId: "com.spell.three"
+  },
+  expo: {
+    extra: {
+      eas: {
+        projectId: "dcaa7171-39ab-41d8-a95c-10ed45658225"
+      }
+    }
+  }
+}
+});
