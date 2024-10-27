36a34117577829ddd8a15123475541bc1e8f448c
Sami Pellinen <sami@pell.fi>
2024-10-27 17:19:00 +0200

    Fix app.config.json structure

--- app.config.js
+++ app.config.js
@@ -1,11 +1,10 @@
 export default ({ config }) => ({
   ...config,
-  android: {
-    ...config.android,
-    package: "com.spell.three",
-    applicationId: "com.spell.three"
-  },
   expo: {
+    android: {
+        package: "com.spell.three",
+        applicationId: "com.spell.three"
+    },
     extra: {
       eas: {
         projectId: "dcaa7171-39ab-41d8-a95c-10ed45658225"
