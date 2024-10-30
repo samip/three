4c6e347a0f7f3b3a45ea65c598b2c26b2ac7cb08
Sami Pellinen <sami.m.pellinen@gmail.com>
2024-10-30 14:29:50 +0200

    Take matrix render mesh as child prop

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,43 @@
 import { StyleSheet, View, Text } from 'react-native';
 import {  Canvas } from "@react-three/fiber";
 import Matrix from '@/components/Matrix';
-
+import Box from '@/components/Box';
+import * as THREE from 'three';
+import { useEffect, useRef } from 'react';
 
 export default function HomeScreen() {
+  const materials = [
+    new THREE.MeshStandardMaterial({ color: "red" }), // Right face
+    new THREE.MeshStandardMaterial({ color: "green" }), // Left face
+    new THREE.MeshStandardMaterial({ color: "blue" }), // Top face
+    new THREE.MeshStandardMaterial({ color: "yellow" }), // Bottom face
+    new THREE.MeshStandardMaterial({ color: "cyan" }), // Front face
+    new THREE.MeshStandardMaterial({ color: "magenta" }), // Back face
+  ];
+  const mesh = useRef<THREE.Mesh>(null!);
+
+  useEffect(() => {
+    const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    // Add the boxMesh as a child of the mesh
+    mesh.current = boxMesh;
+    console.log('index', mesh.current);
+  }, []);
+  // add THREE.BoxGeomeotry to mesh.current.children
+
+
+  const circleGeometry = new THREE.CircleGeometry(0.5, 32); // change to circle
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const circleMesh = new THREE.Mesh(circleGeometry, material);
+  
   return (
     <View style={{ flex: 1 }}>
       <Canvas  style={{ flex: 1, backgroundColor: '#3b3b3b' }}>
-        <Matrix />
+        <Matrix xSize={8} ySize={8}>
+          {circleMesh}
+        </Matrix>
       </Canvas>
     </View>
     )
--- components/Matrix.tsx
+++ components/Matrix.tsx
@@ -1,25 +1,72 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useMemo } from "react";
 import { useFrame, useThree, MeshProps } from "@react-three/fiber";
 import * as THREE from "three";
 import { Gyroscope } from "expo-sensors";
 import { Platform } from "react-native";
-// import { Renderable } from "../types/renderable";
-import Box from './Box'; // Adjust the path as necessary
-import { blue } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
-
+import { Children } from 'react';
+import Box from './Box';
 
 interface MatrixProps {
-  renderItem: MeshProps;
+  xSize: number;
+  ySize: number;
+  children?:THREE.Mesh;
 }
 
-export default function Matrix() {
+export default function Matrix({children, xSize, ySize}: MatrixProps) {
   const ref = useRef<THREE.Mesh>(null!);
+  console.log('Children', children);
   const rotation = useRef({ x: 0, y: 0, z: 0 });
-
   const { scene, camera } = useThree();
+  
+  const onChildFrame = (mesh: THREE.Mesh, x: number, y: number) => {
+    const maxDistanceToCenter = Math.sqrt(Math.pow(-3.5, 2) + Math.pow(-3, 2));
+    const distanceToCenter = Math.sqrt(Math.pow(x - 3.5, 2) + Math.pow(y - 3, 2));
+    (mesh.material as THREE.Material).opacity = distanceToCenter / maxDistanceToCenter;
+    mesh.rotation.x = rotation.current.x;
+    mesh.rotation.y = rotation.current.y;
+    mesh.rotation.z = rotation.current.z;
+  }
+
+  const getBoxMesh = () => {
+    const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    return new THREE.Mesh(boxGeometry, boxMaterial);
+  }
+
+  const getDefaultMesh = () => getBoxMesh();
+
+  const onChildLoad = (mesh: THREE.Mesh, x: number, y: number) => {
+    (mesh.material as THREE.Material).opacity = 0;
+  }
+
+  // Create memoized array of boxes
+  const boxes = useMemo(() => {
+    const boxArray: THREE.Mesh[][] = []; // array of arrays of Mesh3D
+    for (let x = 0; x < xSize; x++) {
+      boxArray[x] = [];
+      for (let y = 0; y < ySize; y++) {
+        const staticProps = {
+          onLoad: onChildLoad,
+          onFrame: onChildFrame,
+          key: `${x}_${y}`,
+          position: [x, y, 0]
+        };
+
+        const matrixMesh = children ? children.clone() : getDefaultMesh();
+        matrixMesh.position.set(staticProps.position[0], staticProps.position[1], staticProps.position[2]);
+        boxArray[x].push(matrixMesh);
+        scene.add(matrixMesh);
+      }}
+      
+    return boxArray;
+  }, [xSize, ySize]);
 
   useEffect(() => {
     let subscription: any;
+    const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    ref.current = boxMesh;
 
     if (Platform.OS !== "web") {
       Gyroscope.setUpdateInterval(20); // Update interval in milliseconds
@@ -42,7 +89,7 @@ export default function Matrix() {
       }
     };
   }, []);
-  
+
   useFrame(() => {
     if (ref.current) {
       ref.current.rotation.x += rotation.current.x;
@@ -51,30 +98,9 @@ export default function Matrix() {
     }
   });
 
-  const onChildFrame = (mesh:THREE.Mesh, x:any, y:any) => {  
-    const maxDistanceToCenter = Math.sqrt(Math.pow(-3.5, 2) + Math.pow(-3, 2));
-    const distanceToCenter = Math.sqrt(Math.pow(x - 3.5, 2) + Math.pow(y - 3, 2));
-    (mesh.material as THREE.Material).opacity = distanceToCenter / maxDistanceToCenter;
-    mesh.rotation.x = rotation.current.x;
-    mesh.rotation.y = rotation.current.y; 
-    mesh.rotation.z = rotation.current.z;
-  }
-
-  const onChildLoad = (mesh:THREE.Mesh, x:any, y:any) => {
-    (mesh.material as THREE.Material).opacity = 0;
-  }
-    
-
   return (
     <React.Fragment>
       <ambientLight />
-      {[0, 1, 2, 3, 4, 5, 6, 7].map((x) => (
-        <React.Fragment key={x}>
-          {[0, 1, 2, 3, 4, 5, 6, 7].map((y) => (
-            <Box onLoad={onChildLoad} onFrame={onChildFrame} key={`${x}_${y}`} position={[x, y, 0]} />
-          ))}
-        </React.Fragment>
-      ))}
     </React.Fragment>
   );
 }
