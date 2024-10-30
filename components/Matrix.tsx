ef64b032114e1b9e469c0fbba060324fc6874673
Sami Pellinen <sami.m.pellinen@gmail.com>
2024-10-30 14:56:03 +0200

    Matrix with different z coordinate for first and last item

--- components/Matrix.tsx
+++ components/Matrix.tsx
@@ -14,18 +14,24 @@ interface MatrixProps {
 
 export default function Matrix({children, xSize, ySize}: MatrixProps) {
   const ref = useRef<THREE.Mesh>(null!);
-  console.log('Children', children);
   const rotation = useRef({ x: 0, y: 0, z: 0 });
   const { scene, camera } = useThree();
   
-  const onChildFrame = (mesh: THREE.Mesh, x: number, y: number) => {
-    const maxDistanceToCenter = Math.sqrt(Math.pow(-3.5, 2) + Math.pow(-3, 2));
-    const distanceToCenter = Math.sqrt(Math.pow(x - 3.5, 2) + Math.pow(y - 3, 2));
-    (mesh.material as THREE.Material).opacity = distanceToCenter / maxDistanceToCenter;
-    mesh.rotation.x = rotation.current.x;
-    mesh.rotation.y = rotation.current.y;
-    mesh.rotation.z = rotation.current.z;
+
+  const onChildBeforeRender = (mesh: THREE.Mesh, x: number, y: number) => {
+    // console.log('onChildBeforeRender', x, y);
   }
+
+  const onChildLoad = (mesh: THREE.Mesh, x: number, y: number) => {
+    console.log('onChildLoad', x, y);
+    if (x == 0 && y == 0) {
+      mesh.position.set(x, y, -0.5)
+    }
+    if (x == xSize - 1 && y == ySize - 1) {
+      mesh.position.set(x, y, 0.5)
+    }
+  }
+
 
   const getBoxMesh = () => {
     const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
@@ -35,38 +41,28 @@ export default function Matrix({children, xSize, ySize}: MatrixProps) {
 
   const getDefaultMesh = () => getBoxMesh();
 
-  const onChildLoad = (mesh: THREE.Mesh, x: number, y: number) => {
-    (mesh.material as THREE.Material).opacity = 0;
-  }
-
   // Create memoized array of boxes
   const boxes = useMemo(() => {
     const boxArray: THREE.Mesh[][] = []; // array of arrays of Mesh3D
     for (let x = 0; x < xSize; x++) {
       boxArray[x] = [];
       for (let y = 0; y < ySize; y++) {
-        const staticProps = {
-          onLoad: onChildLoad,
-          onFrame: onChildFrame,
-          key: `${x}_${y}`,
-          position: [x, y, 0]
-        };
 
         const matrixMesh = children ? children.clone() : getDefaultMesh();
-        matrixMesh.position.set(staticProps.position[0], staticProps.position[1], staticProps.position[2]);
+        matrixMesh.position.set(x, y, 0);
+        matrixMesh.onBeforeRender = () => {
+          onChildBeforeRender(matrixMesh, x, y);
+        }
         boxArray[x].push(matrixMesh);
         scene.add(matrixMesh);
+        onChildLoad(matrixMesh, x, y);
       }}
-      
+
     return boxArray;
   }, [xSize, ySize]);
 
   useEffect(() => {
     let subscription: any;
-    const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    ref.current = boxMesh;
 
     if (Platform.OS !== "web") {
       Gyroscope.setUpdateInterval(20); // Update interval in milliseconds
