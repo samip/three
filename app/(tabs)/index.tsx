8e4e57cd2be365c78ecf646f5b78faa53d107600
Sami Pellinen <sami@pell.fi>
2024-11-03 09:02:28 +0200

    Web sphere

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,38 +9,13 @@ import CameraFollow from '@/components/CameraFollow';
 import CursorLight from '@/components/CursorLight';
 import { useTexture } from '@react-three/drei';
 
-// Create a component for the textured mesh
-function TexturedMesh() {
-  const [mesh, setMesh] = useState<THREE.Mesh | null>(null);
-
-  useEffect(() => {
-    const textureLoader = new THREE.TextureLoader();
-    textureLoader.load('/assets/textures/bricks.jpg', (texture) => {
-      const material = new THREE.MeshBasicMaterial({ map: texture });
-      const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-      const newMesh = new THREE.Mesh(boxGeometry, material);
-      setMesh(newMesh);
-    });
-  }, []);
-
-  return mesh ? mesh : new THREE.Mesh();
-}
-
 export default function HomeScreen() {
-  const materials = [
-    new THREE.MeshStandardMaterial({ color: "red" }), // Right face
-    new THREE.MeshStandardMaterial({ color: "green" }), // Left face
-    new THREE.MeshStandardMaterial({ color: "blue" }), // Top face
-    new THREE.MeshStandardMaterial({ color: "yellow" }), // Bottom face
-    new THREE.MeshStandardMaterial({ color: "cyan" }), // Front face
-    new THREE.MeshStandardMaterial({ color: "magenta" }), // Back face
-  ];
-
   const sphereGeometry = new THREE.SphereGeometry(5); 
   const material = new THREE.MeshStandardMaterial({ 
     roughness: 0.7,
     metalness: 0.1
   });
+
   const sphereMesh = new THREE.Mesh(sphereGeometry, material);
   
   return (
@@ -51,7 +26,7 @@ export default function HomeScreen() {
           <CursorLight />
           <ambientLight intensity={0.2} />
           <Matrix padding={0.1} xSize={8} ySize={8}>
-            {TexturedMesh()}
+            {sphereMesh}
           </Matrix>
         </CameraFollow>
       </Canvas>
--- components/Matrix.tsx
+++ components/Matrix.tsx
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect, useMemo } from "react";
+import React, { useRef, useEffect, useMemo, useState } from "react";
 import { useFrame, MeshProps } from "@react-three/fiber";
-import { Stats } from "@react-three/drei";
+import { Stats, useTexture } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import * as THREE from "three";
 
@@ -14,7 +14,21 @@ interface MatrixProps {
 
 export default function Matrix({children, xSize, ySize, padding = 0, renderHelpers = false}: MatrixProps) {
   const { scene } = useThree();
+  const [colorMap, alphaMap] = useTexture([
+    '/assets/textures/bricks.jpg',
+    '/assets/textures/netmesh.png'
+  ]);
+
   const itemSideLength = 1;
+
+  const getBoxMesh = () => {
+    const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    return new THREE.Mesh(boxGeometry, boxMaterial);
+  }
+
+  const getDefaultMesh = () => getBoxMesh();
+  const [mesh, setMesh] = useState<THREE.Mesh>(getDefaultMesh());
 
   const drawSphere = (position: THREE.Vector3, radius: number) => {
     const sphereGeometry = new THREE.SphereGeometry(radius, 32, 32);
@@ -31,31 +45,38 @@ export default function Matrix({children, xSize, ySize, padding = 0, renderHelpe
   const onChildLoad = (mesh: THREE.Mesh, x: number, y: number) => {
   
   }
+  
+  useEffect(() => {
+    if (children) {
+      const material = new THREE.MeshBasicMaterial();
+      material.transparent = true
 
+      material.alphaMap = alphaMap;
+      mesh.material = material;
+      children && (children.material = material);
+      setMesh(children);
+    }
+  }, [children]);
+  
   useFrame(() => {
 
   });
 
 
-  const getBoxMesh = () => {
-    const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    return new THREE.Mesh(boxGeometry, boxMaterial);
-  }
 
-  const getDefaultMesh = () => getBoxMesh();
   
   // scale mesh to fit in 1x1x1 cube
   const scaleToFit = (mesh: THREE.Mesh) => {
-    let boundingBox = mesh.geometry.boundingBox; // object.geometry.computeBoundingBox();
-    if (!boundingBox) {
+    if (!mesh.geometry.boundingBox) {
       mesh.geometry.computeBoundingBox();
     }
-    boundingBox = mesh.geometry.boundingBox;
-    if (!boundingBox) {
+
+    if (!mesh.geometry.boundingBox) {
       console.error('Could not compute bounding box');
       return;
     }
+
+    const boundingBox = mesh.geometry.boundingBox;
     const max = Math.max(boundingBox.max.x - boundingBox.min.x, boundingBox.max.y - boundingBox.min.y, boundingBox.max.z - boundingBox.min.z);
     const padding = 0.2;
     const scale = (itemSideLength - padding)/ max;
@@ -64,16 +85,18 @@ export default function Matrix({children, xSize, ySize, padding = 0, renderHelpe
 
   const boxes = useMemo(() => {
     const boxArray: THREE.Mesh[][] = [];
+    
     for (let x = 0; x < xSize; x++) {
-      boxArray[x] = [];
+      boxArray[x] = new Array(ySize);
       for (let y = 0; y < ySize; y++) {
-        const matrixMesh = children ? children.clone() : getDefaultMesh();
-        scaleToFit(matrixMesh);
-        matrixMesh.onBeforeRender = () => {
-          onChildBeforeRender(matrixMesh, x, y);
+        scaleToFit(mesh);
+
+        // matrixMesh.material = material;
+        mesh.onBeforeRender = () => {
+          onChildBeforeRender(mesh, x, y);
         }
-        boxArray[x].push(matrixMesh);
-        onChildLoad(matrixMesh, x, y);
+        boxArray[x].push(mesh.clone());
+        onChildLoad(mesh, x, y);
       }}
     return boxArray;
   }, [xSize, ySize]);
