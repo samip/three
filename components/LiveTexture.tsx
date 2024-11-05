25b53dfc831510adcff7a182046f7b6401a6b7c6
Sami Pellinen <sami.m.pellinen@gmail.com>
2024-11-05 15:59:23 +0200

    wip live texture

--- components/LiveTexture.tsx
+++ components/LiveTexture.tsx
@@ -1,66 +1,95 @@
+import { Float } from 'react-native/Libraries/Types/CodegenTypes';
 import * as THREE from 'three';
 
-const width = 32;
-const height = 32;
+const width = 128;
+const height = 128;
 
 export enum LiveTextureType {
-    BLACK_AND_WHITE,
-    FULL_COLOR,
-    FULL_GREEN,
+  BLACK_AND_WHITE,
+  FULL_COLOR,
+  FULL_GREEN,
+  DIAGONAL,
 }
 
-function getTextureImageData(type: LiveTextureType) {
-    const size = width * height;
-    const data = new Uint8ClampedArray(4 * size);
-    const alpha = 255;
-    // Fill with random black or white pixels
-    for (let i = 0; i < size; i++) {
-        const stride = i * 4;
-        switch (type) {
-            case LiveTextureType.BLACK_AND_WHITE:
-                const value = Math.random() > 0.5 ? 255 : 0;
-                data[stride] = value;     // r
-                data[stride + 1] = value; // g
-                data[stride + 2] = value; // b
-                data[stride + 3] = alpha; // a (useless)
-                break;
-            case LiveTextureType.FULL_COLOR:
-                data[stride] = Math.floor(Math.random() * 256);  // r
-                data[stride + 1] = Math.floor(Math.random() * 256); // g
-                data[stride + 2] = Math.floor(Math.random() * 256); // b
-                data[stride + 3] = alpha; // a (useless)
-                break;
-            case LiveTextureType.FULL_GREEN:
-                data[stride] = 0;
-                data[stride + 1] = 255;
-                data[stride + 2] = 0;
-                data[stride + 3] = alpha;
-                break;
-        }
+function getTextureImageData(type: LiveTextureType, position: Float = 0.0) {
+  const size = width * height;
+  const data = new Uint8ClampedArray(4 * size);
+  const alpha = 255;
+  // Fill with random black or white pixels
+  for (let i = 0; i < size; i++) {
+    const stride = i * 4;
+    switch (type) {
+      case LiveTextureType.BLACK_AND_WHITE:
+        const value = Math.random() > 0.5 ? 255 : 0;
+        data[stride] = value;     // r
+        data[stride + 1] = value; // g
+        data[stride + 2] = value; // b
+        data[stride + 3] = alpha; // a (useless)
+        break;
+      case LiveTextureType.FULL_COLOR:
+        data[stride] = Math.floor(Math.random() * 256);  // r
+        data[stride + 1] = Math.floor(Math.random() * 256); // g
+        data[stride + 2] = Math.floor(Math.random() * 256); // b
+        data[stride + 3] = alpha; // a (useless)
+        break;
+      case LiveTextureType.FULL_GREEN:
+        data[stride] = 0;
+        data[stride + 1] = 255;
+        data[stride + 2] = 0;
+        data[stride + 3] = alpha;
+        break;
+      case LiveTextureType.DIAGONAL:
+        const color = (x: number, y: number, position: Float) => {
+          let pos = Math.floor((width * position) - (width / 2));
+          const distanceFromDiagonal = Math.abs( (x + pos) - (y + pos) );
+          return distanceFromDiagonal <= stripeWidth ? 0 : 255;
+        };
+
+        const stripeWidth = 32;
+        const x = i % width;
+        const y = Math.floor(i / width);
+        console.log(x, y);
+        // generate diagonal black and white strip
+        const colorValue = color(x, y, position);
+        data[stride] = colorValue;
+        data[stride + 1] = colorValue;
+        data[stride + 2] = colorValue;
+        data[stride + 3] = alpha;
+        break;
     }
-    return data;
+  }
+  return data;
 }
 
-export function generateLiveTexture(type: LiveTextureType) {
-    const data = getTextureImageData(type);
-    const texture = new THREE.DataTexture(
-        data,
-        width,
-        height,
-        THREE.RGBAFormat
-    );
-    texture.needsUpdate = true;
-    return texture;
+export function generateLiveTexture(type: LiveTextureType, position: Float = 0.0) {
+  const data = getTextureImageData(type, position);
+  const texture = new THREE.DataTexture(
+    data,
+    width,
+    height,
+    THREE.RGBAFormat
+  );
+  texture.needsUpdate = true;
+  return texture;
 }
 
-export function getCanvas(type: LiveTextureType) {
-    const imageData = new ImageData(getTextureImageData(type), 32, 32);
-    const canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext("2d");
-    if (context) {
-        context.putImageData(imageData, 0, 0);
-    }
-    return canvas;
+export function getCanvas(type: LiveTextureType, position: Float = 0.0) {
+  const imageData = new ImageData(getTextureImageData(type, position), width, height);
+  const canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  const context = canvas.getContext("2d");
+  if (context) {
+    context.putImageData(imageData, 0, 0);
+  }
+  return canvas;
+}
+
+export function dumpTexture(type: LiveTextureType, position: Float = 0.0) {
+  const canvas = getCanvas(type, position);
+  document.body.prepend(canvas);
+}
+
+if (require.main === module) {
+  console.log(getCanvas(LiveTextureType.FULL_COLOR));
 }
