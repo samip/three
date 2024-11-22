cefe3baf1abca347cde7446db88737dc44ed6654
Sami Pellinen <sami@pell.fi>
2024-11-22 02:37:24 -0800

    Prettier

--- app.config.js
+++ app.config.js
@@ -2,13 +2,13 @@ export default ({ config }) => ({
   ...config,
   expo: {
     android: {
-        package: "com.spell.three",
-        applicationId: "com.spell.three"
+      package: 'com.spell.three',
+      applicationId: 'com.spell.three',
     },
     extra: {
       eas: {
-        projectId: "dcaa7171-39ab-41d8-a95c-10ed45658225"
-      }
-    }
-  }
+        projectId: 'dcaa7171-39ab-41d8-a95c-10ed45658225',
+      },
+    },
+  },
 });
--- metro.config.js
+++ metro.config.js
@@ -4,16 +4,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-[("js", "jsx", "json", "ts", "tsx", "cjs", "mjs")].forEach((ext) => {
-    if(config.resolver.sourceExts.indexOf(ext) === -1){
-        config.resolver.sourceExts.push(ext);
-    }
+[('js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs')].forEach((ext) => {
+  if (config.resolver.sourceExts.indexOf(ext) === -1) {
+    config.resolver.sourceExts.push(ext);
+  }
 });
 
-["glb", "gltf", "png", "jpg", "jsmat"].forEach((ext) => {
-    if(config.resolver.assetExts.indexOf(ext) === -1){
-        config.resolver.assetExts.push(ext);
-    }
+['glb', 'gltf', 'png', 'jpg', 'jsmat'].forEach((ext) => {
+  if (config.resolver.assetExts.indexOf(ext) === -1) {
+    config.resolver.assetExts.push(ext);
+  }
 });
 
 module.exports = config;
