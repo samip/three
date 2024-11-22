7605827cccd481bce7e8692040c54e41ef97a70d
Sami Pellinen <sami@pell.fi>
2024-11-22 02:19:03 -0800

    lint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: ['expo', 'prettier'],
   ignorePatterns: ['tools/', 'lib/'],
   rules: {
     indent: ['warn', 2],
   },
-  plugins: ['prettier-plugin-organize-imports'],
 };
