6e24e7cce9b38b0ebacd02ed6d15714b6f2764cd
Sami Pellinen <sami@pell.fi>
2024-11-22 01:55:17 -0800

    eslint rules + added prettier-plugin-organize-imports plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: 'expo',
-  ignorePatterns: ['tools/', "lib"],
+  ignorePatterns: ['tools/', 'lib/'],
+  rules: {
+    indent: ['warn', 2],
+  },
+  plugins: ["prettier-plugin-organize-imports"],
 };
