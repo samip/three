d1650eef959cbcaccea43a8b727350c7ceff8120
Sami Pellinen <sami@pell.fi>
2024-11-26 17:54:25 +0200

    Disable unused variable/Parameters warning if name starts with _

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,15 @@ module.exports = {
   ignorePatterns: ['tools/', 'lib/'],
   rules: {
     indent: ['warn', 2, { SwitchCase: 1 }],
-    'react/no-unknown-property': 'off'
+    'react/no-unknown-property': 'off',
+    "no-unused-vars": "off", // broken with react-fiber, disabled for now
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ]
   },
 };
