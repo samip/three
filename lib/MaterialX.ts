8a0bd22e9455cf15df70a8e25b8a848b23f3e7de
Sami Pellinen <sami@pell.fi>
2024-12-02 00:08:18 +0200

    Glass shark

--- lib/MaterialX.ts
+++ lib/MaterialX.ts
@@ -1,26 +1,26 @@
-import { THREE } from 'expo-three';
-import data from '../assets/materials/carpaint.matx.json';
-import threeUniforms from './shaders/gltf_pbr_gold/uniforms';
-import vShader from './shaders/gltf_pbr_gold/vertex';
-import fShader from './shaders/gltf_pbr_gold/fragment';
+import { THREE } from "expo-three";
+import data from "../assets/materials/carpaint.matx.json";
+import threeUniforms from "./shaders/gltf_pbr_gold/uniforms";
+import vShader from "./shaders/gltf_pbr_gold/vertex";
+import fShader from "./shaders/gltf_pbr_gold/fragment";
 
-export function getMaterialXTexture(radianceTexture: THREE.Texture, irradianceTexture: THREE.Texture) {
+export function getMaterialXTexture(
+  radianceTexture: THREE.Texture,
+  irradianceTexture: THREE.Texture
+) {
   const otherUniforms = {
     u_numActiveLightSources: { value: 0 },
     // u_lightData: { value: lightData },
-    u_envMatrix: { value:
-        new THREE.Matrix4().makeRotationY(Math.PI / 2)
+    u_envMatrix: {
+      value: new THREE.Matrix4().makeRotationY(Math.PI / 2),
     },
     u_envRadiance: { value: radianceTexture },
     u_envRadianceMips: {
-        value:
+      value:
         Math.trunc(
-            Math.log2(
-                Math.max(
-                    radianceTexture.image.width,
-                    radianceTexture.image.height,
-                ),
-            ),
+          Math.log2(
+            Math.max(radianceTexture.image.width, radianceTexture.image.height)
+          )
         ) + 1,
     },
     u_envRadianceSamples: { value: 16 },
@@ -29,19 +29,19 @@ export function getMaterialXTexture(radianceTexture: THREE.Texture, irradianceTe
   };
 
   const uniforms = {
-      ...threeUniforms,
-      ...otherUniforms,
+    ...threeUniforms,
+    ...otherUniforms,
   };
   const materialParams = {
-      glslVersion: THREE.GLSL3,
-      uniforms: uniforms,
-      vertexShader: vShader,
-      fragmentShader: fShader,
-      transparent: false,
-      blendEquation: THREE.AddEquation,
-      blendSrc: THREE.OneMinusSrcAlphaFactor,
-      blendDst: THREE.SrcAlphaFactor,
-      side: THREE.DoubleSide,
+    glslVersion: THREE.GLSL3,
+    uniforms: uniforms,
+    vertexShader: vShader,
+    fragmentShader: fShader,
+    transparent: false,
+    blendEquation: THREE.AddEquation,
+    blendSrc: THREE.OneMinusSrcAlphaFactor,
+    blendDst: THREE.SrcAlphaFactor,
+    side: THREE.DoubleSide,
   };
   return new THREE.RawShaderMaterial(materialParams);
 }
