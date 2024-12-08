ffc1ac812e9a44694f6ac96c3c4f27ca0c70eb2a
Sami Pellinen <sami@pell.fi>
2024-12-09 00:57:34 +0200

    Remove app/index.js which broke production build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,15 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import { suppressWarnings } from "./suppressWarnings";
+
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  suppressWarnings();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
